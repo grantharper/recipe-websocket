dbc6d443fdbfe2af87271d2f599922e69c0eb2dd
grantharper <grant.e.harper@gmail.com>
2019-01-09 16:22:03 -0500

    add next step button

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,22 +1,9 @@
 var stompClient = null;
 
-function setConnected(connected) {
-  $("#connect").prop("disabled", connected);
-  $("#disconnect").prop("disabled", !connected);
-  if (connected) {
-    $("#recipeInfo").show();
-  }
-  else {
-    $("#recipeInfo").hide();
-  }
-  $("#steps").html("");
-}
-
 function connect() {
   var socket = new SockJS('/websocket');
   stompClient = Stomp.over(socket);
   stompClient.connect({}, function (frame) {
-    setConnected(true);
     console.log('Connected: ' + frame);
     stompClient.subscribe('/recipe/websocket', function (recipe) {
       var parsed = JSON.parse(recipe.body);
@@ -25,27 +12,38 @@ function connect() {
   });
 }
 
-function disconnect() {
-  if (stompClient !== null) {
-    stompClient.disconnect();
-  }
-  setConnected(false);
-  console.log("Disconnected");
+function initializeRecipe() {
+  stompClient.send("/app/update", {}, JSON.stringify({'currentStepIndex': '0'}));
 }
 
-function sendName() {
-  stompClient.send("/app/update", {}, JSON.stringify({'currentStepIndex': $("#stepIndex").val()}));
+function nextStep() {
+  var pageStepIndex = $("#currentStep").attr('data-index');
+  console.log('currentStepIndex=' + pageStepIndex);
+  var numberPageStepIndex = parseInt(pageStepIndex);
+  console.log('numberPageStepIndex=', numberPageStepIndex);
+  var nextStepIndex = numberPageStepIndex + 1;
+  console.log('nextIndex=', nextStepIndex);
+  $("#currentStep").attr('data-index', nextStepIndex.toString());
+  stompClient.send("/app/update", {}, JSON.stringify({'currentStepIndex': nextStepIndex.toString()}));
 }
 
 function showCurrentStep(message) {
-  $("#steps").append("<tr><td>" + message + "</td></tr>");
+  console.log('get message=' + message);
+  $("#currentStep").html(message);
 }
 
 $(function () {
   $("form").on('submit', function (e) {
     e.preventDefault();
   });
-  $( "#connect" ).click(function() { connect(); });
-  $( "#disconnect" ).click(function() { disconnect(); });
-  $( "#send" ).click(function() { sendName(); });
+  // $( "#connect" ).click(function() { connect(); });
+  // $( "#disconnect" ).click(function() { disconnect(); });
+  // $( "#send" ).click(function() { sendStep(); });
+  $("#nextStep").click(function () {
+    nextStep();
+  });
+  $("#initializeRecipe").click(function () {
+    initializeRecipe();
+  });
+  connect();
 });
