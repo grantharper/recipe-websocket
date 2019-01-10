6ac562f09533cbad8c12090521e70bcd42bbd1de
grantharper <grant.e.harper@gmail.com>
2019-01-10 16:27:47 -0500

    write external page that can call an http endpoint to update the step at the broker

---
+++ src/main/resources/static/external.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+  $("#updateStepIndex").click(function () {
+    console.log('updateStepIndex clicked');
+    callUpdateEndpoint();
+  });
+
+});
+
+
+function callUpdateEndpoint() {
+  var stepIndex = $("#stepIndex").val();
+  console.log('sending request with stepIndex=', stepIndex);
+
+  var stepIndexUpdate = {
+    currentStepIndex: stepIndex
+  };
+
+  console.log('data=', stepIndexUpdate);
+
+  $.ajax({
+    url: '/alexa',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify(stepIndexUpdate),
+    success: function (data) {
+      console.log('response=', data);
+    },
+    error: function (jq, textStatus, errorThrown) {
+      console.log(jq);
+      console.log('response error=', textStatus);
+      console.log('thrown: ', errorThrown);
+    }
+  });
+}
