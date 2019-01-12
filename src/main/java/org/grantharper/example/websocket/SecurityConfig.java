f8a75cb994b35e533af95f08493bd0e6276d23ad
grantharper <grant.e.harper@gmail.com>
2019-01-12 14:04:43 -0500

    disable csrf and auth to allow for the POST request to /alexa to work without auth

--- src/main/java/org/grantharper/example/websocket/SecurityConfig.java
+++ src/main/java/org/grantharper/example/websocket/SecurityConfig.java
@@ -2,11 +2,11 @@ package org.grantharper.example.websocket;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
@@ -22,15 +22,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return manager;
     }
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/alexa").permitAll()
-//                .anyRequest().authenticated();
-//                .and()
-//                .formLogin()
-//                .loginPage("/login").permitAll()
-//                .and()
-//                .logout().permitAll();
-//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/alexa").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+
+    }
+
 }
